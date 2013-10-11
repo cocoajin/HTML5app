7d8ffb2ac22d397efe1774b249c3d87672a720a7
cocoajin <dev.keke@gmail.com>
2013-10-11 17:20:27 +0800

    add checkBoxRadio

--- HTML5app/RootViewController.m
+++ HTML5app/RootViewController.m
@@ -43,6 +43,7 @@
 
     listArray = [[NSMutableArray alloc]initWithCapacity:10];
     
+    [listArray addObject:@"Checkboxradio Widget Exampl"];
     [listArray addObject:@"JQMobile button 按钮"];
     [listArray addObject:@"弹出对话框"];
     [listArray addObject:@"一个页面多Page结构"];
@@ -53,6 +54,7 @@
     
     
     webArray = [[NSMutableArray alloc]initWithCapacity:10];
+    [webArray addObject:@"checkBox"];
     [webArray addObject:@"button"];
     [webArray addObject:@"diag"];
     [webArray addObject:@"mutalPage"];
